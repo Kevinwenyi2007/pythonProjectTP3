703db3e6ff9013d16cc4522223c5a0beed9f8302
weny <weny@educ.college-montreal.qc.ca>
2023-11-27 14:37:43 -0500

    hello

--- main.py
+++ main.py
@@ -20,22 +20,27 @@ startingScore = 20
 print('ton niveau de vie au debut est',startingScore)
 
 
-while Jeu == 'yes':
+while Jeu == 'yes' and startingScore > 0: # l'utilisateur veut jouer le jeu et s'il n'est pas mort
     scoreMonstre1 = random.randint(1, 6)
     scoreMonstre2 = random.randint(1, 6)
 
     scoreMonstre = scoreMonstre1 + scoreMonstre2
 
+    # si L'utilisateur gagne plus trois fois cosecutivement, il va combattre avec un plus grand monstre(le boss)
+    if victoiresConsecutive >= 3:
+        scoreMonstre3 = random.randint(7, 12)
+        scoreMonstre4 = random.randint(7, 12)
+        scoreMonstre = scoreMonstre3 + scoreMonstre4
+
     print('le score de votre adversaire est de', scoreMonstre)
+
     print('Que voulez-vous faire: combattre cet adversaire(choix 1), Contourner cet adversaire et aller ouvrir une autre porte(choix 2), Afficher les règles du jeu(choix 3), Quitter la partie(choix 4)')
 
     choix = int(input('cest quoi votre choix'))
 
-    if startingScore <= 0:
-        break
 
 
-    if choix == 1:
+    if choix == 1: #Que voulez-vous faire: combattre cet adversaire
         score_de = random.randint(1, 30)
         if scoreMonstre >= score_de:
             nombre_defaites += 1
@@ -52,25 +57,19 @@ while Jeu == 'yes':
             print('votre nombre de victoire est de', nombre_victoire)
             print('votre nombre de victoire consecutive est de', victoiresConsecutive)
     numero_combat += 1
-    print('cest le',numero_combat,'combat')
+    print('cest le',numero_combat,'combat') # montrer le nombre de combat
 
-    # si L'utilisatuer gagne plus trois fois cosecutivement, il va combattre avec un plus grand monstre #question
-    if victoiresConsecutive >= 3:
-        scoreMonstre3 = random.randint(7, 12)
-        scoreMonstre4 = random.randint(7, 12)
-        score2 = scoreMonstre3 + scoreMonstre4
-        print(score2)
 
-    if choix == 2:
+    if choix == 2: # Contourner cet adversaire et aller ouvrir une autre porte
         startingScore -= 1
         print(startingScore,'est votre nombre de vie maintenant')
-    if choix == 3:
+    if choix == 3:#Afficher les règles du jeu
         print(
             'Pour réussir un combat, il faut que la valeur du dé lancé soit supérieure à la force de l’adversaire.  Dans ce cas, le niveau de vie de usager est augmenté de la force de adversaire.Une défaite a lieu lorsque la valeur du dé lancé par usager est inférieure ou égale à la force de adversaire.  Dans ce cas, le niveau de vie de usager est diminué de la force de adversaire.La partie se termine lorsque les points de vie de usager tombent sous 0.Usager peut combattre ou éviter chaque adversaire, dans le cas de évitement, il y a une pénalité de 1 point de vie. ')
 
-    if choix == 4:
+    if choix == 4:# Quitter la partie
         print('merci et au revoir')
-        break
+        Jeu = 'no' # arreter le jeu
 else:
     print('merci et au revoir')
 # pour la question theorique, la reponse est que on peut creer plusieurs variables qui representent les forces
